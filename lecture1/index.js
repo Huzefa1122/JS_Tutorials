77cadff7b5d465f7f9963dbb77c45d1e08d47e3f
huzefa1122 <muhammadhuzefa2003@gmail.com>
2024-08-07 16:30:59 +0500

    First lecture

---
+++ lecture1/index.js
@@ -0,0 +1,31 @@
+var var3 
+// Data types
+
+" STRING "
+23
+23.23
+true || false
+undefined
+null
+
+// Print Something 
+
+console.log(" STRING ", 23, 23.23, true, false, undefined, null)
+console.log("Hello World!")
+
+//  Variables 
+// If we dont give value of var1 program error deta h 
+// var1  
+//Hoisting  
+let var2  
+var var3 
+var3="Jhinga lala"
+var3=61
+var1=false 
+//const var4=""
+// var4="Somethign else"
+//Constant is something that doesnt change
+console.log(var1)
+console.log(var2)
+console.log(var3)
+//console.log(var4)
